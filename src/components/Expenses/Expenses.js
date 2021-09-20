3a2589d5d3f840e16240c8bfa07bd8490a0c6446
Sourav34 <borateourav@gmail.com>
2021-09-20 18:44:57 +0530

    react expense tracker

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,28 @@
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
+import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+export default Expenses;
